192de00341ef59c963635de82940133cd6e219e0
Ravi Shankar <57468568+raviShankarpisini@users.noreply.github.com>
2024-07-05 08:21:56 +0530

    updating to latest version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import DestinationSearch from './components/DestinationSearch'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './App.css'
 
-export default App;
+const destinationsList = [
+  {
+    id: 1,
+    name: 'Melaka Mosque',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/melaka-mosque-img.png',
+  },
+  {
+    id: 2,
+    name: 'Shrubland',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/shrubland-img.png',
+  },
+  {
+    id: 3,
+    name: 'New York',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/new-york-img.png',
+  },
+  {
+    id: 4,
+    name: 'Escarpment',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/escarpment-img.png',
+  },
+  {
+    id: 5,
+    name: 'Westminster Abbey',
+    imgUrl:
+      'https://assets.ccbp.in/frontend/react-js/westminster-abbey-img.png',
+  },
+  {
+    id: 6,
+    name: 'South Downs National Park',
+    imgUrl:
+      'https://assets.ccbp.in/frontend/react-js/south-downs-national-park-img.png',
+  },
+  {
+    id: 7,
+    name: 'National Historic Site',
+    imgUrl:
+      'https://assets.ccbp.in/frontend/react-js/national-historic-site-img.png',
+  },
+  {
+    id: 8,
+    name: 'Tower Bridge',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/tower-bridge-img.png',
+  },
+  {
+    id: 9,
+    name: 'Arc Here',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/arc-here-img.png',
+  },
+  {
+    id: 10,
+    name: 'Steeple',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/steeple-img.png',
+  },
+  {
+    id: 11,
+    name: 'Glaciokarst',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/glaciokarst-img.png',
+  },
+  {
+    id: 12,
+    name: 'Parco Nazionale delle Cinque Terre',
+    imgUrl:
+      'https://assets.ccbp.in/frontend/react-js/parco-nazionale-delle-cinque-terre-img.png',
+  },
+]
+
+const App = () => <DestinationSearch destinationsList={destinationsList} />
+
+export default App
